03b155a4ed395524fb564cbf604bd32e8af89e79
jsalaiz <jonathan.salaiz@gmail.com>
2021-08-09 21:26:54 -0400

    Finished the exercise

---
+++ VariableExercise/VariableExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dog = "Clyde";
+            int age = 4;
+            char firstLetterOfName = 'C';
+            bool isDog = true;
+            double ageInDogYears = age * 7;
+            decimal HumanYearsDivedByDogYears = age / 7;
+
+            Console.WriteLine($"My dog's name is {dog} he is {age} years old. In Dog Years he is {ageInDogYears} years old. {firstLetterOfName}, {HumanYearsDivedByDogYears}, {(isDog ? "yes" : "no")}");
+        }
+    }
+}
